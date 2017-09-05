680946a100aaac308c158077e8f25caebdac5756
simmarin <s.mtz.marin@gmail.com>
2017-09-04 21:43:06 -0700

    crear metodos en la clase "Operaciones" -metodo multiplicacion -metodo division

--- src/c1/Operaciones.java
+++ src/c1/Operaciones.java
@@ -1,5 +1,13 @@
 package c1;
 
 public class Operaciones {
-    
+    public static double multiplicacion(double a){
+        double b=kb*1024;
+        return b;
+    }
+    public static double division(double a){
+        double b=mb/1024;
+        return b;
+    }
+
 }
