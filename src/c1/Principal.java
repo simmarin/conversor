c8638326f836ab386c730bc091bc1238b69263cd
simmarin <s.mtz.marin@gmail.com>
2017-09-04 22:05:28 -0700

    primera operacion completada

--- src/c1/Principal.java
+++ src/c1/Principal.java
@@ -3,6 +3,8 @@ package c1;
 public class Principal {
 
     public static void main(String[] args) {
-
+Operaciones o =new Kb_Mb();
+        System.out.println("son " +o.multiplicacion(1)+" kb");
+        System.out.println("son " +o.division(55)+" mb");
     }
 }
